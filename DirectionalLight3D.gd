65030a5c42316f34e3dfe77aea391a14ca651cd0
Craig Berry <csberry123@outlook.com>
2024-01-09 13:19:47 +0000

    Added a reverse_depth mask to normal outlines to clean up some artifacts. Cleaned up code a touch

--- DirectionalLight3D.gd
+++ DirectionalLight3D.gd
@@ -1,5 +1,5 @@
 @tool
 extends DirectionalLight3D
 
-func _process(delta):
+func _process(_delta):
 	(%PostProcessing as MeshInstance3D).mesh.surface_get_material(0).set_shader_parameter('light_direction', -global_basis.z)
